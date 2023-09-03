4d979a5ec37ee30cb31d97a02646b6f017db4fdc
Bilal Salim <bilall1999775@outlook.com>
2023-09-03 10:52:10 +0300

    css commit

---
+++ network/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path('newpost/', views.newpost, name='newpost'),
+    path('profile/<int:user_ident>', views.profile, name='profile'),
+
+]
